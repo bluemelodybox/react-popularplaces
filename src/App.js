a2713292423167832f80df26e2a854fb67d6293c
Wesley Chew <wesleychewjj@gmail.com>
2020-04-14 10:12:35 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./App.css";
 const axios = require("axios");
 
 function App() {
-  useEffect(() => {
-    axios.get("/data/").then((res) => {
-      console.log(res);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("/data/").then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
 
   return (
     <div className="App">
