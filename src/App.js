bd9ae2bd0fd0c074a14a2a90ad36ebd64e1f6638
Wesley Chew <wesleychewjj@gmail.com>
2020-04-15 22:18:27 +0800

    test

--- src/App.js
+++ src/App.js
@@ -55,16 +55,19 @@ function App() {
       ) : (
         <Container maxWidth="xl">
           <Grid container spacing={1}>
-            <div style={{ position: "absolute", right: 24 }}>
-              <h4>Last update: {lastUpdatedTime}</h4>
-            </div>
+            {/* <div style={{ position: "absolute", right: 24 }}>
+              
+            </div> */}
             <Grid item lg={12} xs={12}>
               <h2 align="center">Places of Interest</h2>
               <Typography align="center" variant="body2" color="textSecondary">
                 Data acquired from Google popular times, updated every 15 minutes
               </Typography>
+              <Typography align="center" variant="body2" color="textSecondary">
+                Last update: {lastUpdatedTime}
+              </Typography>
             </Grid>
-            <Grid item lg={3} xs={12} style={{ marginTop: "12px" }}>
+            <Grid item lg={3} xs={12}>
               <h3 align="center">Parks</h3>
               <div className="cards">
                 {lineData.slice(0, 5).map((item, key) => (
@@ -72,7 +75,7 @@ function App() {
                 ))}
               </div>
             </Grid>
-            <Grid item lg={3} xs={12} style={{ marginTop: "12px" }}>
+            <Grid item lg={3} xs={12}>
               <h3 align="center">Markets & Food Centers</h3>
               <div className="cards">
                 {lineData.slice(5, 10).map((item, key) => (
@@ -80,7 +83,7 @@ function App() {
                 ))}
               </div>
             </Grid>
-            <Grid item lg={3} xs={12} style={{ marginTop: "12px" }}>
+            <Grid item lg={3} xs={12}>
               <h3 align="center">Shopping Malls</h3>
               <div className="cards">
                 {lineData.slice(10, 15).map((item, key) => (
@@ -88,7 +91,7 @@ function App() {
                 ))}
               </div>
             </Grid>
-            <Grid item lg={3} xs={12} style={{ marginTop: "12px" }}>
+            <Grid item lg={3} xs={12}>
               <h3 align="center">MRT Stations</h3>
               <div className="cards">
                 {lineData.slice(15, 20).map((item, key) => (
@@ -99,6 +102,7 @@ function App() {
             <Grid item lg={12}>
               <div className="header">
                 <h4 style={{ marginBottom: 0 }}>Map of Singapore showing changes in crowd</h4>
+                <h4>Last update: {lastUpdatedTime}</h4>
               </div>
               <div className="legend">
                 <p style={styles.CircleGreen}></p>
